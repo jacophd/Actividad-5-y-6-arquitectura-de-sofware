45874539cfe8c5f3c4f939235e44a7118a756593
jacophd <80134394+jacophd@users.noreply.github.com>
2023-04-16 12:16:21 -0500

    Update acrofobia.js

--- assets/js/acrofobia.js
+++ assets/js/acrofobia.js
@@ -36,8 +36,8 @@ function analizar(){
     var criterio_1 = parseInt (document.getElementById("criterio_1").value);
     var criterio_2 = parseInt (document.getElementById("criterio_2").value);
     var criterio_3 = parseInt (document.getElementById("criterio_3").value);
-    var criterio_4 = parseInt (document.getElementById("criterio_3").value);
-    var criterio_5 = parseInt (document.getElementById("criterio_3").value);
+    var criterio_4 = parseInt (document.getElementById("criterio_4").value);
+    var criterio_5 = parseInt (document.getElementById("criterio_5").value);
     var criterios_acrofobia = criterio_1 + criterio_2 + criterio_3 + criterio_4 + criterio_5;
 
     if(criterios_acrofobia == 1) {
